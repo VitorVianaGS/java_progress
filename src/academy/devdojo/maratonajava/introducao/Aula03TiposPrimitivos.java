353ea7b1534ed9dc1ca4fa785fa3ea606f06c614
VitorVianaGS <20vitorgabriel@gmail.com>
2025-12-11 11:39:52 -0300

    Implementação básica de variáveis

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivos.java
@@ -1,4 +1,20 @@
 package academy.devdojo.maratonajava.introducao;
+/*
+Pratic
 
+Create variables for the fields bellow between <>
+and print the next mensage:
+
+Me <name>, living at the address <address> confirm
+to received the salary about <salary>, in date <date>.
+*/
 public class Aula03TiposPrimitivos {
+    static void main() {
+        String name = "Vitor";
+        String address = "Av. Paulista, 123, Centro, São Paulo";
+        double salary = 40000;
+        String salaryReceivementDate = "10/01/2026";
+        String relatory = "Me "+name+" living at the address "+address+" confirm to received the salary about "+salary+", in date "+salaryReceivementDate;
+        System.out.println(relatory);
+    }
 }

328d26d9ce02c460a3d153d417f638c9020990e6
VitorVianaGS <20vitorgabriel@gmail.com>
2025-12-23 13:09:51 -0300

    Descrição de estruturas condicionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcolica!");
+        }
+        // ! negação seria o mesmo de == false.
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida!");
+        }
+        // quando for usar uma estrutura condicional if, é obrigatório usar uma expressão que retorne true ou false.
+        // if (idade); vai dar erro
+
+        boolean c = false;
+        if (c == true) {
+            System.out.println("Algo que nunca deve ser feito");
+        }
+    }
+}

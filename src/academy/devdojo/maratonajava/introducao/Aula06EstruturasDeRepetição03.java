ae7fc51a996870db7fe3a83f3876fe1bb7b2b33f
VitorVianaGS <20vitorgabriel@gmail.com>
2026-01-04 13:18:00 -0300

    Correção de commit

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição03.java
@@ -1,4 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepetição03 {
+    public static void main(String[] args) {
+        // Imprimir numeros de 0 a 50 porém, dando Break no 25.
+        byte i = 0;
+        while (i <= 50){
+            if (i > 25) {
+                break;
+            }
+            System.out.println("i = "+ i++);
+        }
+    }
 }

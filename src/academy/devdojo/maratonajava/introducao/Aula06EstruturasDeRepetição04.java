cc0bdd65432d963c1556116e7575a304307a79ea
VitorVianaGS <20vitorgabriel@gmail.com>
2026-01-04 16:12:28 -0300

    Código mais limpo do mesmo exercício

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepetição04.java
@@ -4,21 +4,13 @@ public class Aula06EstruturasDeRepetição04 {
     // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
     // Condição valorParcela >= 1000
     public static void main(String[] args) {
-        double qtdParcelas = 0;
-        double valorParcela = 5000;
-        double valorCarro = 38000;
-        double iterador = 0;
-        while (valorParcela < valorCarro) {
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
             if (valorParcela < 1000) {
-                System.out.println("Não é aceito um valor abaixo de R$ 1000,00 por parcela");
-                break;
-            } else if (iterador < valorCarro) {
-                qtdParcelas++;
-                iterador += valorParcela;
-            } else {
-                System.out.printf("A quantidade de parcelas permitida no valor de R$" + valorCarro +" é de " + qtdParcelas);
                 break;
             }
+            System.out.println("parcela "+ parcela+" R$ "+valorParcela);
         }
     }
 }

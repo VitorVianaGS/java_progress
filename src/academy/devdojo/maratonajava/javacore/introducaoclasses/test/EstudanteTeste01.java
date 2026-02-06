5772f517ddd2ef9b18237b82f55cb24c1d33eade
Vitor Viana <vitorbagnuoloviana@gmail.com>
2026-02-06 14:11:42 -0300

    Classes

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTeste01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.domain.Pessoa;
+
+public class PessoaTest01 {
+    static void main(String[] args) {
+        Estudante
+    }
+}

61b9e821e4b5759f5b6d3e910bf17e255c61ae70
VitorVianaGS <20vitorgabriel@gmail.com>
2026-02-09 17:01:31 -0300

    Passagem de parâmetros e argumentos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    static void main() {
+        Calculadora caclculadora = new Calculadora();
+        caclculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
